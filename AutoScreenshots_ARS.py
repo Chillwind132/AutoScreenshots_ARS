78752ed61ea83c22e6437d0888b794b44b2f012b
Chillwind132 <99220831+Chillwind132@users.noreply.github.com>
2022-03-19 12:01:41 -0400

    Add files via upload

---
+++ AutoScreenshots_ARS.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import img2pdf
+
+def save_screenshots(selection):
+    
+    with open("list_urls.txt") as file:
+        for line in file:
+            URL = line
+
+            if selection == "1":
+                format = ".jpg"
+            else:
+                format = ".jp2"
+
+            mod_line = line.replace("/", "").replace(".html", "").replace(".", "").replace("https", "").replace("http", "").replace(":", "").replace("www", "")
+            file_name = path + "\\output\\" + mod_line.strip() + format
+            print(file_name)
+            options = webdriver.ChromeOptions()
+            options.headless = True
+            driver = webdriver.Chrome(options=options)
+            driver.get(URL)
+            def S(X): return driver.execute_script(
+                'return document.body.parentNode.scroll'+X)
+            driver.set_window_size(S('Width'), S('Height'))
+            driver.find_element_by_tag_name('body').screenshot(file_name)
+
+    driver.quit()
+
+
+def convert_img_pdf(selection):
+    for filename in os.listdir(output_path):
+        f_img = os.path.join(output_path, filename)
+        if os.path.isfile(f_img):
+            f_out = f_img.removesuffix(".jp2") + ".pdf"
+            print(f_img, f_out)
+            with open(f_out, "wb") as f:
+                f.write(img2pdf.convert(f_img))
+        if selection == "2":
+            os.remove(f_img)
+
+if __name__ == "__main__":
+
+    path = os.getcwd()
+    output_path = path + "\\output\\"
+    if os.path.exists(output_path):
+        print("Dir exists")
+    else:
+        os.makedirs(output_path)
+
+    selection = input(
+        "Would you like to convert URLS to images or PDFs? Press 1 to select Images; Press 2 to select PDFs\n")
+
+    save_screenshots(selection)
+
+
+    if selection == "2":
+        convert_img_pdf(selection)
